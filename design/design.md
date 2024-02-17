95fd56ec50eb6c1f611286036662bc80e454f549
fxhxyz <fxhxyz@proton.me>
2024-02-17 10:59:47 +0200

    update

--- design/design.md
+++
@@ -1,9 +0,0 @@
-#
-
-### [Dribbble](https://dribbble.com/shots/20849370-StartPage-Desktop)
-
-#
-
-### [Figma](https://www.figma.com/file/CuQbvxrgQeTE3fnYYde2gl/StartPage?type=design&node-id=0%3A1&t=kjmbcbsqf3Vt36Fo-1)
-
-#
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,42 @@
-const main = document.querySelector('.main__list');
 import data from '../../data/user.json';
 
-let svgUrl = `./symbol-defs.14665cf3.svg#icon`;
-let markup = ``;
+const main = document.querySelector('.main__list');
+const time = document.querySelector('.main__time');
+const link = document.querySelectorAll('.footer__link');
 
-function getData() {
-	const valuesArray = Object.values(data);
+let svgUrl = './symbol-defs.4c6fa1d0.svg#icon';
+let markup = '';
 
-	for (const item of valuesArray) {
-		let values = Object.values(item);
+link.forEach((l) => {
+  l.addEventListener('click', (e) => {
+    let categories = e.currentTarget.id;
 
-		renderHtml(values);
-	}
+    if (categories === 'home') {
+      time.style.display = 'block';
+    } else {
+      time.style.display = 'none';
+    }
+
+    main.innerHTML = '';
+
+    getData(categories);
+  })
+});
+
+const getData = (categories) => {
+	const valuesObj = data[categories];
+
+	for (const value of Object.values(valuesObj)) renderHtml(value);
 }
 
-function renderHtml(values) {
-	const [name, link, svgName, width = 50, height = 50] = values;
+const renderHtml = (value) => {
+	const { name, link, svg, width = 50, height = 50 } = value;
 
 	markup = `
-    <li class="main__item">
-      <a class='main__link' href='https://${link}' target='_blank' rel='noopener noreferrer nofollow'>
+    <li class='main__item animate__animated animate__fadeIn animate__slower' title='${name}'>
+      <a class='main__link' href='http://${link}' target='_blank' rel='noopener noreferrer nofollow'>
         <svg class='main__svg' width='${width}' height='${height}' aria-label='${name} logo'>
-          <use xlink:href='${svgUrl}-${svgName}'></use>
+          <use xlink:href='${svgUrl}-${svg}'></use>
         </svg>
       </a>
     </li>
@@ -29,5 +44,3 @@ function renderHtml(values) {
 
 	main.insertAdjacentHTML('beforeend', markup);
 }
-
-getData();
